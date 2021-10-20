694008e87df84ec5f2057ad085634a8affc938f5
Edmar <edmar_psn@hotmail.com>
2021-10-20 16:44:20 -0300

    update

---
+++ etapas.js
@@ -0,0 +1,27 @@
+let etapas = [
+    {
+        titulo: "PRESIDENTE",
+        numeros: 4,
+        candidatos: [
+
+            {
+                numero: '3456',
+                nome: "Joao Doria",
+                partido: "tico",
+                fotos: [
+                    {url: "Untitled-1.jpg", legenda: "Presidente"},
+                    {url: "Untitled-2.jpg", legenda: "Vice-Presidente", small: true}
+                ]
+            },
+            {
+                numero: '4896',
+                nome: "Joao Dkopooria",
+                partido: "tihgco",
+                fotos: [
+                    {url: "Untitled-3.jpg", legenda: "Presidente"},
+                    {url: "Untitled-4.jpg", legenda: "Vice-Presidente", small: true}
+                ]
+            },
+        ]
+    }
+]
