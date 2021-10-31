07f2a54ae122c5d65ee5b31ff95c98feda593478
Edmar <edmar_psn@hotmail.com>
2021-10-31 13:28:50 -0300

    update

--- etapas.js
+++ etapas.js
@@ -5,23 +5,32 @@ let etapas = [
         candidatos: [
 
             {
-                numero: '3456',
-                nome: "Joao Doria",
-                partido: "tico",
+                numero: '7692',
+                nome: "João Maria",
+                partido: "Melhor",
                 fotos: [
                     {url: "Untitled-1.jpg", legenda: "Presidente"},
                     {url: "Untitled-2.jpg", legenda: "Vice-Presidente", small: true}
                 ]
             },
             {
-                numero: '4896',
-                nome: "Joao Dkopooria",
-                partido: "tihgco",
+                numero: '1199',
+                nome: "Joao Ricardo",
+                partido: "Quebra-Cabeça",
                 fotos: [
                     {url: "Untitled-3.jpg", legenda: "Presidente"},
                     {url: "Untitled-4.jpg", legenda: "Vice-Presidente", small: true}
                 ]
             },
+            {
+                numero: '9250',
+                nome: "Mariana Joal",
+                partido: "Exitoso",
+                fotos: [
+                    {url: "Untitled-5.jpg", legenda: "Presidente"},
+                    {url: "Untitled-6.jpg", legenda: "Vice-Presidente", small: true}
+                ]
+            }
         ]
     }
 ]
